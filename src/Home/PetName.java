8641421864944a0523a4c85ae527d265b3afea18
LytaNicoll <lyta.nicoll@gmail.com>
2018-11-09 13:54:53 -0600

    PetNames

---
+++ src/Home/PetName.java
@@ -0,0 +1,10 @@
+package Home;
+
+public class PetName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print("Pip, Pop, Pearl, and crustation. They're shrimp.");
+	}
+
+}
