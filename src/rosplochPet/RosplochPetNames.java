fc04292e0e382b347be990d74029b00a4adbb56e
Josh Rosploch <43151586+Josh-Rosploch@users.noreply.github.com>
2018-11-09 14:05:13 -0600

    Update RosplochPetNames.java

--- src/rosplochPet/RosplochPetNames.java
+++ src/rosplochPet/RosplochPetNames.java
@@ -2,6 +2,6 @@ package rosplochPet;
 
 public class RosplochPetNames {
 	public static void main(String[] args) {
-		System.out.println("Kato, Rigby, and Milo.")
+		System.out.println("Kato, Rigby, and Milo.");
 	}
 }
